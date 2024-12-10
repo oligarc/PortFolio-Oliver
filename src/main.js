e0aacc8503d3e5e357676583e7147d592f01f43e
oligarc <oliver25399@gmail.com>
2024-12-10 13:21:24 +0100

    tocar carrusel

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
-import "./styles/main.scss";
+import "./styles/main.scss"; // Tu archivo SCSS
+import Glide from "@glidejs/glide"; // Importar Glide.js
+import "@glidejs/glide/dist/css/glide.core.min.css"; // Estilos de Glide.js
+
+document.addEventListener("DOMContentLoaded", () => {
+  const glide = new Glide(".glide", {
+    type: "carousel",
+    startAt: 0,
+    perView: 1,
+    gap: 10,
+    focusAt: "center",
+    breakpoints: {
+      800: {
+        perView: 1,
+      },
+    },
+  });
+
+  glide.mount();
+});
